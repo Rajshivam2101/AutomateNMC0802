dd7aaa7733018647145239b46f7a8a3233588145
Shivam Singh <rajshivamsingh2101@gmail.com>
2026-02-20 14:43:07 +0530

    added one class to practice programm

---
+++ src/main/java/programpractice/ReverseWithPreDmethods.java
@@ -0,0 +1,14 @@
+package programpractice;
+
+public class ReverseWithPreDmethods {
+
+
+    public static void main(String[] args) {
+
+
+        String name = "Shivam Singh RAjput";
+
+        StringBuilder sb = new StringBuilder(name);
+        System.out.println(sb.reverse());
+    }
+}
